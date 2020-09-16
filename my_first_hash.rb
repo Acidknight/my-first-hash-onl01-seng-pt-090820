eec0eb3574fe3e22dd97b2270d6c3471ec560ff7
Anthony Leon <acidknights89@gmail.com>
2020-09-16 02:48:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   video_game_release_years = {
-    "Super Metroid" == "1993"
+    "Super Metroid" => "1994"
     
   }
 
